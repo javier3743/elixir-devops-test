b4f3174e7c159d821923ceb57daa65708c675340
Javier Palacios <javierpala2020@gmail.com>
2024-09-15 17:52:49 -0500

    Fix: test create backend

--- infra/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = {{ secrets.TF_VAR_backend_bucket }}
-    key    = "terraform.tfstate"
-    region = {{ secrets.TF_VAR_region }}
-    dynamodb_table = {{ secrets.TF_VAR_backend_key_table }}
-  }
-}
